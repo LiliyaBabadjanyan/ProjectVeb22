9731ae6ac00d229a793afb79e5cc6975d69b3338
LiliyaBabadjanyan <liliyafrein380@gmail.com>
2024-01-20 23:46:04 +0300

    tarifs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ServicesScrin from "./сomponents/Services/ServiceScreen/ServicesScrin";
 import SupportScreen from "./сomponents/support/supportScreen/SupportScreen";
 import BurgerMenu from "./сomponents/burgerMenu/BurgerMenu";
 import ExpirienceScreen from "./сomponents/expirience/ExpirienceScreen";
+import PacksScreen from "./сomponents/Packs/Packs/PacksScreen";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
             <SupportScreen/>
             <BurgerMenu/>
             <ExpirienceScreen/>
+            <PacksScreen/>
         </div>
     );
 }
